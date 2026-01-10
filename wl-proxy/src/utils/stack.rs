af305fd2a4a58cd6ff6916ea5b7dd0621332f87d
Julian Orth <ju.orth@gmail.com>
2026-01-10 15:11:55 +0100

    autocommit 2026-01-10 15:11:55 CET

--- wl-proxy/src/utils/stack.rs
+++ wl-proxy/src/utils/stack.rs
@@ -1,37 +1,45 @@
-use std::{cell::UnsafeCell, mem};
+use std::{
+    cell::{Cell, UnsafeCell},
+    mem,
+};
 
 pub(crate) struct Stack<T> {
     vec: UnsafeCell<Vec<T>>,
+    borrowed: Cell<bool>,
 }
 
 impl<T> Default for Stack<T> {
     fn default() -> Self {
         Self {
             vec: Default::default(),
+            borrowed: Default::default(),
         }
     }
 }
 
 impl<T> Stack<T> {
-    pub(crate) fn pop(&self) -> Option<T> {
-        // SAFETY: This is a leaf function.
-        let vec = unsafe { &mut *self.vec.get() };
-        vec.pop()
+    #[inline(always)]
+    fn with<U>(&self, f: impl FnOnce(&mut Vec<T>) -> U) -> U {
+        if self.borrowed.replace(true) {
+            std::process::abort();
+        }
+        // SAFETY: The borrowed flag ensures that there is only ever one reference.
+        let res = f(unsafe { &mut *self.vec.get() });
+        self.borrowed.set(false);
+        res
     }
 
+    #[inline(always)]
+    pub(crate) fn pop(&self) -> Option<T> {
+        self.with(|vec| vec.pop())
+    }
+
+    #[inline(always)]
     pub(crate) fn push(&self, v: T) {
-        // SAFETY: The allocator invoked by Vec::push could theoretically call back into
-        //         this object but it's not something we worry about here.
-        //         We could do a `mem::replace` dance if capacity == len but then we'd
-        //         still have to rely on the allocator only being invoked in those
-        //         situations, which is undocumented.
-        let vec = unsafe { &mut *self.vec.get() };
-        vec.push(v);
+        self.with(|vec| vec.push(v))
     }
 
     pub(crate) fn take(&self) -> Vec<T> {
-        // SAFETY: This is a a leaf function.
-        let vec = unsafe { &mut *self.vec.get() };
-        mem::take(vec)
+        self.with(mem::take)
     }
 }
